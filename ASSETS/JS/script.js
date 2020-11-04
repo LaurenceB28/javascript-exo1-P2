16fdaef3fc6772a0301ba967395fbcfdc1034cb8
LaurenceB28 <laurence.brd@gmail.com>
2020-11-04 15:15:16 +0100

    exo1P2JS

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,9 @@
+function setNewImage() 
+{
+    document.getElementById("img1").src = "ASSETS/IMG/none.jpg";
+}
+
+function setOldImage()
+{
+    document.getElementById("img1").src = "ASSETS/IMG/bonne-soeur.jpg";
+}
